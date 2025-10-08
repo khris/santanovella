f64f882704a6b337546ac6501b74abdb81e0a7d8
Segi Hong <segi.hong88@linecorp.com>
2025-10-08 16:11:18 +0900

    fix(python): remove dict from default value
    Do not use mutable type as default value. It is shared among every function call or class initialization.

--- src/santanovella/mime/mimetype.py
+++ src/santanovella/mime/mimetype.py
@@ -4,7 +4,7 @@ from typing import MutableMapping
 class MimeType:
     type: str = 'text'
     subtype: str = 'plain'
-    parameters: MutableMapping[str, str] = {}
+    parameters: MutableMapping[str, str] = None
 
     def __init__(self, payload: str):
         chunks = payload.split(';')
@@ -12,22 +12,20 @@ class MimeType:
             return
 
         self.type, self.subtype = chunks[0].split('/')
+        self.parameters = {}
 
-        if len(chunks) == 1:
-            return
-
-        try:
-            for chunk in chunks[1:]:
-                k, v = chunk.split('=', 1)
-                self.parameters[k.strip()] = v.strip()
-
-        except ValueError:
-            raise ValueError(f'MIME type parameters should be '
-                             f'"<parameter>=<value>" with ";" as separator, '
-                             f'but {chunks[1:]} is not')
+        if len(chunks) > 1:
+            try:
+                for chunk in chunks[1:]:
+                    k, v = chunk.split('=', 1)
+                    self.parameters[k.strip()] = v.strip()
+            except ValueError:
+                raise ValueError(f'MIME type parameters should be '
+                                 f'"<parameter>=<value>" with ";" '
+                                 f'as separator, but {chunks[1:]} is not')
 
         if self.type == 'text' and 'charset' not in self.parameters:
-            self.parameters['charset'] = 'US-ASCII'
+            self.parameters['charset'] = 'utf-8'
 
     @property
     def charset(self) -> str:
