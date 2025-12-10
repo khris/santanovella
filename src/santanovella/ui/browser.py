34dc6389b333be9df5e569a5e1b7d3f3c2995ea3
khris <khris@khrislog.net>
2025-12-11 08:19:49 +0900

    feat: support canvas resize

--- src/santanovella/ui/browser.py
+++ src/santanovella/ui/browser.py
@@ -4,14 +4,12 @@ import tkinter
 from collections.abc import Iterable
 from copy import copy
 from dataclasses import astuple, dataclass
-from tkinter import EventType
+from tkinter import Event, EventType
 
 from .. import html
 from ..protocol.common import Response, Url
 
 MAX_REDIRECTION = 2
-WIDTH = 800
-HEIGHT = 600
 SCROLL_STEP = 100
 DIV_GAP = 27
 
@@ -36,14 +34,14 @@ class Browser:
         self.window.bind('<Down>', lambda e: self.do_scroll(e, SCROLL_STEP))
         self.window.bind('<Up>', lambda e: self.do_scroll(e, -SCROLL_STEP))
         self.window.bind('<MouseWheel>', lambda e: self.do_scroll(e, SCROLL_STEP))
-        self.canvas = tkinter.Canvas(
-            self.window,
-            width=WIDTH,
-            height=HEIGHT,
-        )
-        self.canvas.pack()
+        self.canvas = tkinter.Canvas(self.window)
+        self.canvas.pack(fill='both', expand=True)
+        self.canvas.bind('<Configure>', lambda e: self.on_resize(e))
         self.display_list = []
         self.scroll = 0
+        self.body = ''
+        self.width = 0
+        self.height = 0
 
     def show_content_from(self, url: Url):
         curr_url = url
@@ -70,17 +68,18 @@ class Browser:
         else:
             logging.warning('redirected %d times, stopped' % MAX_REDIRECTION)
 
-        self.display_list = self.layout(body)
+        self.body = body
+        self.display_list = self.layout()
         self.draw()
 
     def draw(self):
         for char in self.display_list:
             scrolled_pos = Vec2(char.pos.x, char.pos.y - self.scroll)
-            if not is_in(0, 0, WIDTH, HEIGHT, scrolled_pos):
+            if not is_in(0, 0, self.width, self.height, scrolled_pos):
                 continue
             self.canvas.create_text(astuple(scrolled_pos), text=char.char)
 
-    def do_scroll(self, e, step):
+    def do_scroll(self, e: Event, step):
         match e.type:
             case EventType.MouseWheel:
                 self.scroll += e.delta * step * 0.1
@@ -92,13 +91,18 @@ class Browser:
         self.canvas.delete('all')
         self.draw()
 
-    @staticmethod
-    def layout(text) -> Iterable[Char]:
+    def on_resize(self, e: Event):
+        logging.debug('resize %s: %d * %d' % (e.widget, e.width, e.height))
+        self.width, self.height = e.width, e.height
+        self.layout()
+        self.draw()
+
+    def layout(self) -> Iterable[Char]:
         display_list = []
         step = Vec2(13.0, 18.0)
         cursor = Vec2(*astuple(step))
-        for c in text:
-            if cursor.x >= WIDTH:
+        for c in self.body:
+            if cursor.x >= self.width:
                 cursor.x = step.x
                 cursor.y += step.y
             elif c == '\n':
@@ -112,6 +116,7 @@ class Browser:
 
 def is_in(x, y, width, height, pos: Vec2) -> bool:
     return x <= pos.x <= x + width and y <= pos.y <= y + height
+
 
 def has_attr(obj, attr):
     attr = getattr(obj, attr, None)
